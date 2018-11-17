a0e4d3c18505bac5b6e88359bbae684322c4b78d
とろゝ <toro-ponz@users.noreply.github.com>
2018-11-18 00:29:06 +0900

    fix observe timing

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,39 +1,36 @@
 import { YouTubeLiveOverlayer } from 'app/YouTubeLiveOverlayer'
 
-// initialize after loaded
-window.onload = () => {
-  // extention instance
-  let app: YouTubeLiveOverlayer | null = null
+// extention instance
+let app: YouTubeLiveOverlayer | null = null
 
-  // observe name
-  const CHAT_FRAME_NAME = 'YTD-LIVE-CHAT-FRAME';
+// observe name
+const CHAT_FRAME_NAME = 'YTD-LIVE-CHAT-FRAME';
 
-  // observer
-  const observer: MutationObserver = new MutationObserver(
-    (records: MutationRecord[]) => {
-      for (let i = 0; i < records.length; i++) {
-        // added chat area
-        Array.from(records[i].addedNodes)
-          .filter((node: Node) => node.nodeName === CHAT_FRAME_NAME)
-          .some(() => {
-            app = YouTubeLiveOverlayer.tryNew()
-            return true
-          })
+// observer
+const observer: MutationObserver = new MutationObserver(
+  (records: MutationRecord[]) => {
+    for (let i = 0; i < records.length; i++) {
+      // added chat area
+      Array.from(records[i].addedNodes)
+        .filter((node: Node) => node.nodeName === CHAT_FRAME_NAME)
+        .some(() => {
+          app = YouTubeLiveOverlayer.tryNew()
+          return true
+        })
 
-        // removed chat area
-        Array.from(records[i].removedNodes)
-          .filter((node: Node) => node.nodeName === CHAT_FRAME_NAME)
-          .some(() => {
-            app = null
-            return true
-          })
-      }
+      // removed chat area
+      Array.from(records[i].removedNodes)
+        .filter((node: Node) => node.nodeName === CHAT_FRAME_NAME)
+        .some(() => {
+          app = null
+          return true
+        })
     }
-  );
+  }
+);
 
-  // observe #chat through document body
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-}
+// observe #chat through document body
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
